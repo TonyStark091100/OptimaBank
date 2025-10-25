1145bd43005a048448090653db15f748d3c9d8ae
Gurumani <gurumanint@gmail.com>
2025-10-25 19:55:03 +0800

    backend: relax CSP img-src to allow Unsplash domains so voucher images load

--- backend/middleware.py
+++ backend/middleware.py
@@ -10,7 +10,7 @@ class SecurityHeadersMiddleware(MiddlewareMixin):
             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://accounts.gstatic.com https://apis.google.com; "
             "frame-src 'self' https://accounts.google.com https://accounts.gstatic.com; "
             "connect-src 'self' https://accounts.google.com https://accounts.gstatic.com https://apis.google.com; "
-            "img-src 'self' data: https://*.googleusercontent.com https://ssl.gstatic.com; "
+            "img-src 'self' data: https://*.googleusercontent.com https://ssl.gstatic.com https://images.unsplash.com https://*.unsplash.com; "
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
             "font-src 'self' https://fonts.gstatic.com; "
             "frame-ancestors 'self'"
